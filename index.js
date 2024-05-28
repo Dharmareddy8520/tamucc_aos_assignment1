485765e72102be1c4e3acabd5b237122e69f18ae
Dharmareddy8520 <19B61A0533@nmrec.edu.in>
2024-05-28 03:06:22 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,81 @@
+const details = {
+  Name: "Dharma Reddy Pandem",
+  Email: "dpandem@islander.tamucc.edu",
+  Phone: "+91-9603938735",
+  Location: "Texas, US",
+  Social_links: "linkedin.com/in/dharmareddypandem/",
+  Education: {
+    university: "Texas A&M University-Corpus Christi",
+    location: "Corpus Christi, USA",
+    degree: "Master of Science in Computer Science",
+    dates: "Jan 2024 - May 2026",
+    courses: [
+      "Algorithm analysis & Data Structures",
+      "Design & analysis of Computer Algorithms",
+    ],
+  },
+  experience: {
+    company: "Ai4see, Bangalore",
+    title: "Website Development Intern",
+    dates: "Mar 2023 - Jun 2023",
+    responsibilities: [
+      "Design of the overall architecture of the web application",
+      "Build reusable code and libraries for future use",
+      "Optimization of the application for maximum speed and scalability",
+      "Implementation of security and data protection",
+      "Integration of the front-end and back-end aspects of the web application",
+    ],
+  },
+
+  skills: {
+    programming_languages: ["C++", "Python", "C", "JavaScript", "Java"],
+    technologies: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Bootstrap",
+      "React.Js",
+      "jQuery",
+      "RESTful APIs",
+      "Node.js",
+    ],
+    databases: ["SQL", "MySQL", "MongoDB"],
+    developer_tools: [
+      "Linux",
+      "Git",
+      "VS Code",
+      "Eclipse",
+      "Pycharm",
+      "Android Studio",
+      "OBS",
+    ],
+  },
+  projects: {
+    title: "Real Time Number Plate Detection",
+    description:
+      "The goal of this project is to develop a system for recognizing vehicle number plates using existing surveillance cameras. The system will capture images of vehicles and use image segmentation to extract the number plate region and store and display data in website.",
+    github: "github.com/Dharmareddy8520/ANPR",
+    youtube: "www.youtube.com/watch?v=Owf1Tajbj7k&ab_channel=ramreddy",
+    date: "Apr 2023",
+
+    title: "DIGITALIZED CERTIFICATE GENERATION SYSTEM",
+    description:
+      "It is a web-based application that has the capability to automatically generate certificates in bulk. The primary objective of this project is to streamline the process of certificate generation, making it more efficient and timesaving.",
+    base_paper:
+      "ijsrem.com/download/digitalized-certificate-generation-system/",
+    date: "Dec 2022",
+  },
+  certifications: {
+    c: [
+      "title: Introduction to Programming Using Python",
+      "provider: Udemy",
+      "date: April 2021",
+    ],
+    c1: [
+      "title: Cybersecurity Essentials",
+      "provider: Cisco",
+      "date: July 2023",
+    ],
+  },
+};
+window.details = details; // This line makes `details` a global variable
